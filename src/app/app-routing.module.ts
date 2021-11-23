fa0a79fa286941cafed81e8b34c4135de7afa764
Julian Marin <julyma01@hotmail.com>
2021-11-23 09:52:55 -0500

    version 1.0.0 frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path:"inicio",
+  component: InicioComponent
+},
+{
+  path:"",
+  pathMatch:"full",
+  redirectTo:"/inicio"
+},
+{
+path:"seguridad",
+loadChildren:()=> import("./modulos/seguridad/seguridad.module").then(x=>x.SeguridadModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
