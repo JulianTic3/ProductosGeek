3943e78734a55ce330b739334630e10f3420cf52
Julian Marin <julyma01@hotmail.com>
2021-11-29 15:19:25 -0500

    version 1.0.5 inicio Sesion full

--- src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
+import * as CryptoJS from 'crypto-js';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-identificacion',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdentificacionComponent implements OnInit {
 
-  constructor() { }
+  fgValidador: FormGroup = this.fb.group({
+    'usuario': ['', [Validators.required, Validators.email]],
+    'clave': ['', [Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder,
+    private servicioSeguridad: SeguridadService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  identificarUsuario() {
+    let usuario = this.fgValidador.controls["usuario"].value;
+    let clave = this.fgValidador.controls["clave"].value;
+    let claveCifrada = CryptoJS.MD5(clave).toString();
+    this.servicioSeguridad.identificar(usuario, claveCifrada).subscribe((datos: any) => {
+      this.servicioSeguridad.AlmacenarSesion(datos);
+      this.router.navigate(["/inicio"])
+    }, (error: any) => {
+      //KO
+      alert("Usuario o contraseña inválidos")
+    })
+  }
 }
